bfae0fbbb68e0afa6115aecb03e70a73792419ef
Bitcamp\Bit <werty95190@gmail.com>
2017-11-09 09:42:46 +0900

    aa

--- delicious_sum/src/main/java/com/delip/domain/Restaurant.java
+++ delicious_sum/src/main/java/com/delip/domain/Restaurant.java
@@ -8,6 +8,6 @@ public class Restaurant {
 	//rno, rname, radress, rlat, rlng : Not null
 	//rlat : 위도, rlng : 경도
 	private int rno, rcount, ravg;
-	private String rname, radress, rtel, rhomepage, rlat, rlng, rholiday, rtime;
-					
+	private String rname, radress, rtel, rhomepage, rholiday, rtime;
+	private double rlat, rlng;				
 }
